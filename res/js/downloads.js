cb83c9039be2ffc182cbae600062fad7f2a0a5fa
Konstantin Protzen <code@kondev.de>
2025-02-23 13:24:15 +0100

    ✨ Improve file download section

---
+++ res/js/downloads.js
@@ -0,0 +1,10 @@
+document.getElementById("main-files").onmousemove = e => {
+    for (const card of document.getElementsByClassName("main-filedownload")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
